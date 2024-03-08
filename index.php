18674ce6b9891ee8d286a59115ff7b21eac5b50c
Jed-morris <alegre.jed@gmail.com>
2024-03-08 11:45:07 +0800

    modified:   index.php

--- index.php
+++ index.php
@@ -81,7 +81,7 @@
             <!-- Left and right controls/icons -->
             <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <   span class="visually-hidden">Previous</span>
+                <span class="visually-hidden">Previous</span>
             </button>
 
             <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
