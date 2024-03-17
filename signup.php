8bbc4e386cc95bdb172127ed7dbed6c8f0c03ade
Jed-morris <alegre.jed@gmail.com>
2024-03-18 04:21:11 +0800

    modified:   signup.php

--- signup.php
+++ signup.php
@@ -6,22 +6,22 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
-    <title>Signup- Paws and Whiskers</title>
+    <title>Signup Page- Paws and Whiskers</title>
 
     <style>
     body {
-    background-image: url('img/cats2.jpg');
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    height: 100vh;
+        background-image: url('img/cats2.jpg');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        height: 100vh;
     }
 
     .transparent-card {
-    background-color: rgba(255, 255, 255, 0.7);
-    max-width: 450px;
-    margin: 0 auto;
-    padding: 20px;
+        background-color: rgba(255, 255, 255, 0.7);
+        max-width: 500px;
+        margin: 0 auto;
+        padding: 20px;
     }
     </style>
 </head>
@@ -40,36 +40,39 @@
             <?php } ?>
             <hr>
 
-            <form class="shadow w-450 p3" action="signup1.php" method="post">
-                
-                <div class="mb-7">
-                    <label for="firstname" class="form-label">First Name</label>
-                    <input type="text" class="form-control" id="fname" name="fname" placeholder="First Name" required value="<?php echo (isset($_GET['fname'])) ? $_GET['fname'] : "" ?>">
+            <form class="shadow p-3" action="signup1.php" method="post">
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="firstname" class="form-label">First Name</label>
+                        <input type="text" class="form-control" id="fname" name="fname" placeholder="First Name" required value="<?php echo (isset($_GET['fname'])) ? $_GET['fname'] : "" ?>">
+                    </div>
+                    <div class="col">
+                        <label for="lastname" class="form-label">Last Name</label>
+                        <input type="text" class="form-control" id="lname" name="lname" placeholder="Last Name" value="<?php echo (isset($_GET['lname'])) ? $_GET['lname'] : "" ?>">
+                    </div>
                 </div>
 
-                <div class="mb-7">
-                    <label for="lastname" class="form-label">Last Name</label>
-                    <input type="text" class="form-control" id="lname" name="lname" placeholder="Last Name" value="<?php echo (isset($_GET['lname'])) ? $_GET['lname'] : "" ?>">
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="phone" class="form-label">Phone</label>
+                        <input type="number" class="form-control" id="phone" name="phone" placeholder="Phone number" value="<?php echo (isset($_GET['phone'])) ? $_GET['phone'] : "" ?>">
+                    </div>
+                    <div class="col">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" class="form-control" id="username" name="uname" placeholder="Username" value="<?php echo (isset($_GET['uname'])) ? $_GET['uname'] : "" ?>">
+                    </div>
                 </div>
 
-                <div class="mb-7">
-                    <label for="phone" class="form-label">Phone</label>
-                    <input type="number" class="form-control" id="phone" name="phone" placeholder="Phone number" value="<?php echo (isset($_GET['phone'])) ? $_GET['phone'] : "" ?>">
-                </div>
-
-                <div class="mb-7">
-                    <label for="username" class="form-label">Username</label>
-                    <input type="text" class="form-control" id="username" name="uname" placeholder="Username" value="<?php echo (isset($_GET['uname'])) ? $_GET['uname'] : "" ?>">
-                </div>
-                <div class="mb-7">
+                <div class="mb-3">
                     <label for="email" class="form-label">Email address</label>
                     <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email" value="<?php echo (isset($_GET['email'])) ? $_GET['email'] : "" ?>">
                 </div>
-                <div class="mb-7">
+
+                <div class="mb-3">
                     <label for="password" class="form-label">Password</label>
                     <input type="password" class="form-control" id="password" name="pass" placeholder="Create password" required>
                 </div>
-                <div class="mb-7">
+                <div class="mb-3">
                     <label for="password" class="form-label">Confirm Password</label>
                     <input type="password" class="form-control" id="cpassword" name="cpass" placeholder="Confirm password" required>
                 </div>
